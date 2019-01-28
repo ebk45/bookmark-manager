0e65c151c8bc5c1a90b9e6265db3726280883d9d
ellie turnock <ebturnock@gmail.com>
2019-01-28 17:52:00 +0000

    Test: should show the bookmarks list

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,8 @@
 feature 'Viewing bookmarks' do
-  scenario 'visit the index page' do
-    visit('/')
-    expect(page).to have_content "Hello World"
+  scenario 'view the listed bookmarks' do
+    visit ('/bookmarks')
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.facebook.com"
+    expect(page).to have_content "http://www.youtube.com"
   end
 end
